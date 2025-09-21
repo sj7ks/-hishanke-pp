6c5f84041102499295e943195fdc415a34d3438b
sj7ks <marismynz@gmail.com>
2025-09-21 21:48:49 +0300

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname)); // serve index.html, css, js
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
