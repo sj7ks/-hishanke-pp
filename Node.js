11029b20aab9239f1bda569d180b3a3a3bcbe57b
sj7ks <marismynz@gmail.com>
2025-09-22 13:18:07 +0300

    Node.js

---
+++ Node.js
@@ -0,0 +1,20 @@
+{
+  "name": "uhishanke-app",
+  "version": "1.0.0",
+  "description": "Super professional order app for Christofer's mother",
+  "main": "app.js",
+  "author": "Christofer Robin Münz",
+  "license": "MIT",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.3.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  }
+}
